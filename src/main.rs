c4ffc7195a86d4e3dbfeeaece03b8070150791d9
Renato <renato145@hotmail.com>
2021-11-07 20:20:49 +1030

    add clap

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,27 @@
 use anyhow::Result;
+use clap::Parser;
 use docker_queue::{
     configuration::Settings,
     server::Server,
     telemetry::{get_subscriber, init_subscriber},
 };
 
+#[derive(Debug, Parser)]
+struct Opts {
+    #[clap(subcommand)]
+    subcmd: SubCommand,
+}
+
+#[derive(Debug, Parser)]
+enum SubCommand {
+    Serve,
+}
+
 #[tokio::main(flavor = "current_thread")]
 async fn main() -> Result<()> {
+    let opts = Opts::parse();
+    println!("{:?}", opts);
+
     let subscriber = get_subscriber("docker_queue".into(), "info".into(), std::io::stdout);
     init_subscriber(subscriber);
 
